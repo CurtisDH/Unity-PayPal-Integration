7dfc4071736f5a23b6db44b0f32b017fa71d32e4
Curtis Hodgson <39945904+CurtisDH@users.noreply.github.com>
2020-10-26 22:55:26 +1000

    Preparing to create Editor tool to create orders

---
+++ Assets/Editors/CreateItemTool.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CreateItemTool : EditorWindow
+{
+    [MenuItem("Editors/Tools/CreateOrder")]
+    public static void Init()
+    {
+        CreateItemTool window = (CreateItemTool)EditorWindow.GetWindow(typeof(CreateItemTool));
+        window.minSize = new Vector2(200f, 200f);
+        window.Show();
+    }
+
+    private void OnGUI()
+    {
+
+
+
+
+    }
+
+
+}
